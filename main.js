38be75500bba8deaca1874a6b3ba0b6dc12aa6a6
Ishaan232 <83758075+Ishaan232@users.noreply.github.com>
2022-03-28 15:34:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function crazy(){
+o=Number(document.getElementById("one").value);
+n=Number(document.getElementById("two").value);
+gone=o-n;
+gon=o+n;
+on=o*n;
+one=o/n;
+console.log("Addition",gon);
+console.log("Substraction",gone);
+console.log("Multiplication",on);
+console.log("Division",one);
+}
